d410dbf448ec6be5a71ae3bb8dc08067897e8197
Lylalove <yjjeon0219@gmail.com>
2021-06-22 04:00:54 +0000

    capstone expression and pose change ui code added

---
+++ step5_ui_expression&pose_change/README.md
@@ -0,0 +1,4 @@
+result 폴더에 outputs/step4에 해당하는 결과들을 모두 넣고
+$ chmod +x run.sh
+$ ./run.sh
+
---
+++ step5_ui_expression&pose_change/main.py
@@ -0,0 +1,142 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap,QQuaternion
+from PyQt5 import QtGui
+import pyqtgraph.opengl as gl
+import numpy
+import os
+import requests
+
+import PyQt5
+import sys
+from pywavefront import visualization, Wavefront
+def file_upload():
+    files = {'file': open(os.path.join(os.getcwd(),'input.png'),'rb')}
+    r = requests.post("http://s3.ciplab.ml:5443/", files=files)
+    # print(r.content.decode('utf-8'))
+    f = open("output.obj", "w")
+    f.write(r.content.decode('utf-8'))
+    f.close()
+    # print("gi")
+vertss=[]
+colorss = []
+class MyApp(QWidget):
+    
+    def __init__(self):
+        self.rotation = 0
+        self.d = gl.GLViewWidget()
+        self.bars = []
+        self.m2 = gl.GLMeshItem()
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        mainBox = QHBoxLayout(self)
+        mainBox.addStretch(1)
+        # mainBox.addStretch(2)
+
+        objectBox = QVBoxLayout()
+        controlBox = QVBoxLayout()
+        mainBox.addChildLayout(controlBox)
+        # objectBox.addStretch(1)
+        # objectBox.addStretch(1)
+        # controlBox.addStretch(2)
+        mainBox.addLayout(objectBox)
+        labels = ["Head Position", "Anger", "Contempt", "Disgust", "Fear", "Joy", "Sadness", "Surprise"]
+        for i in range(8):
+            hbox = QHBoxLayout()
+            label = QLabel(labels[i])
+            label.setFixedWidth(100)
+            # label.setMinimumWidth(90)
+            sld = QSlider(Qt.Horizontal)
+            sld.setFixedWidth(420)
+            # sld.set
+            if i== 0:
+                sld.setRange(-45,45)
+                sld.valueChanged[int].connect(self.rotateChange)
+                sld.setValue(0)
+            else:
+                sld.setRange(11*i-11,11*i-1)
+                sld.valueChanged[int].connect(self.expressChanged)
+                # print(sld.objectName())
+                self.bars.append(sld)
+            hbox.addWidget(label)
+            hbox.addWidget(sld)
+            objectBox.addLayout(hbox)
+
+        # objectBox.addWidget(sld)
+        # root_path = os.path.dirname(__file__)
+        # file_upload()
+        # exit(1)
+        imgs_path = "result"
+        sorted_list = sorted(os.listdir(imgs_path))
+        # change the sequence on files in imgs_path
+        for i in range(int(len(sorted_list) / 11)) :
+            select_list = sorted_list[11*i: 11*(i+1)]
+            tempa = select_list[2]
+            select_list[2:10] = select_list[3:11]
+            select_list[10] = tempa
+            sorted_list[11*i: 11*(i+1)] = select_list
+
+        for filename in sorted_list:
+            print(imgs_path + "/" + filename)
+            box = Wavefront(os.path.join(imgs_path, filename))
+            verts = numpy.empty((len(box.mesh_list[0].materials[0].vertices)//18,3,3),dtype=numpy.float32)        
+            colors = numpy.ones((verts.shape[0], 3, 4))
+            for i in range(0,len(box.mesh_list[0].materials[0].vertices)//18):
+                verts[i][0]=box.mesh_list[0].materials[0].vertices[18*i+3:18*i+6]
+                verts[i][1]=box.mesh_list[0].materials[0].vertices[18*i+9:18*i+12]
+                verts[i][2]=box.mesh_list[0].materials[0].vertices[18*i+15:18*i+18]
+                a=box.mesh_list[0].materials[0].vertices[18*i:18*i+3]
+                a.append(1)
+                colors[i][0]=a
+                a[0:3]=box.mesh_list[0].materials[0].vertices[18*i+6:18*i+9]
+                colors[i][1]=a
+                a[0:3]=box.mesh_list[0].materials[0].vertices[18*i+12:18*i+15]
+                colors[i][2]=a
+            vertss.append(verts)
+            colorss.append(colors)
+            if len(vertss) == 1:
+                self.m2 = gl.GLMeshItem(vertexes=verts,vertexColors=colors,smooth=False)
+                # break
+        self.d.addItem(self.m2)
+        self.d.setFixedWidth(1000)
+        self.d.setFixedHeight(1000)
+        # self.d.setCameraPosition(fov=45)
+        # self.d.setCameraPosition(pos=QtGui.QVector3D(0,0,0), distance=10, elevation=0, azimuth=0, rotation=45)
+        # self.d.opts["fov"]=45
+        # print(self.d.opts['vie(wport'])
+        # self.d.opts["rotation"]=(45,1,1,100)
+        self.d.setCameraPosition(distance=5)
+        self.d.opts["rotationMethod"]="quaternion"
+        controlBox.addWidget(self.d)
+        # objectBox
+        self.setLayout(mainBox)
+        self.setWindowTitle('My App')
+        self.setGeometry(0,0,1600,1300)
+        self.show()
+
+
+    def rotateChange(self, value):
+        q = QtGui.QQuaternion.fromEulerAngles(
+                0, value, 0
+        )
+        self.d.opts['rotation'] = q
+        self.d.update()
+
+    def expressChanged(self, value):
+        idx = (value//11)
+        # print(vertss[value])
+        self.m2.setMeshData(vertexes=vertss[value],vertexColors=colorss[value])
+        self.m2.meshDataChanged()
+        for i in range(len(self.bars)):
+            if i != idx:
+                self.bars[i].setValue(i*10)
+                # self.bars[i].update()
+        self.d.update()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
+
